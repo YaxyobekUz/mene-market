3834348534e000ac2344ec544bf5acd2f77a18fa
mryaxyobek <pubgn9642@gmail.com>
2024-01-08 20:40:42 +0500

    added styles of some "flex&grid", "font size" and "button" components

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,8 +6,45 @@ export default {
   ],
   theme: {
     extend: {
+      // colors, bg images
       colors: {
-        'dark': '#111',
+        'primary': {
+          'skyblue': {
+            500: '#08C8F9',
+          },
+          'blue': {
+            700: '#1A3EDD',
+          },
+          'red': {
+            500: '#FE3A30',
+          },
+          'gold': {
+            400: '#FFC436',
+          },
+        },
+      },
+
+      backgroundImage: {
+        'linear-gradient_blue-500': 'linear-gradient(105deg, #08C8F9 0%, #1A3EDD 100%)',
+      },
+
+
+      // font
+      fontSize: {
+        32: '32px',
+        13: '13px',
+      },
+
+      lineHeight: {
+        3.5: '14px',
+        6.5: '26px',
+      },
+
+
+
+      // border
+      borderRadius: {
+        18: '18px',
       },
     },
   },
