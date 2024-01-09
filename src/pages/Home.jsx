144af6f8a9f19340c30bb60e5efeb269811a9b68
mryaxyobek <pubgn9642@gmail.com>
2024-01-09 23:54:58 +0500

    Added new products section for only desktop

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Hero from '../components/Hero';
 import Categories from '../components/Categories';
+import NewProducts from '../components/NewProducts';
 
 const Home = () => {
     return (
         <>
             <Hero />
             <Categories />
+            <NewProducts />
         </>
     )
 };
