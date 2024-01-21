3400bbb444e1136a865472d1a07361d6200c92db
XabibullayevYaxyobek <123488242+mryaxyobek@users.noreply.github.com>
2024-01-21 21:45:23 +0500

    added search form

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,6 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import { Link, useLocation } from 'react-router-dom';
+import { productsData } from '../assets/data';
 
 // images
 import logoImg from '../assets/images/other/logo.png';
@@ -11,6 +12,14 @@ import supportIcon from '../assets/images/svg/support-icon.svg';
 
 
 const Header = () => {
+
+    // search
+    const [searchedProducts, setSearchedProducts] = useState([]);
+    const filteredProducts = (e) => {
+        const searchInput = e.target.querySelector('.js-search-input');
+        const filterProducts = productsData.filter((product) => product.name.toLowerCase().includes(searchInput.value.toLowerCase()));
+        setSearchedProducts(filterProducts);
+    }
     return (
         <header className='sticky top-0 py-6 header-shadow mb-12 max-540:mb-10 bg-white z-10 bg-opacity-70 backdrop-filter_blur-md max-768:py-5 max-640:py-4 max-490:py-3'>
             <div className="container">
@@ -21,29 +30,55 @@ const Header = () => {
                         <img src={logoImg} width={96} height={48} alt="logo image" className="w-24 h-12" />
                     </Link>
 
-                    {/* search form */}
-                    <form
-                        className="flex-center relative grow max-w-[360px] max-768:hidden"
-                        onSubmit={(e) => {
-                            e.preventDefault();
-                        }}
-                    >
-                        <input
-                            type="search"
-                            className="rounded-3xl py-3 pr-14 border border-primary-black-800 px-6 text-medium-18 bg-transparent"
-                            placeholder='Qidirish'
-                        />
-                        <button className="absolute right-6">
-                            <img width={24} height={24} src={searchIcon} alt="search icon" className="w-6 h-6" />
-                        </button>
-                    </form>
+                    {/* search form wrapper */}
+                    <div className="search-form-wrapper">
+                        <form
+                            className="search-form"
+                            title='search'
+                            onSubmit={(e) => {
+                                e.preventDefault();
+                                filteredProducts(e);
+                            }}
+                        >
+
+                            <input
+                                type="search"
+                                className="js-search-input rounded-3xl py-3 pr-14 border border-primary-black-800 px-6 text-medium-18 bg-transparent"
+                                placeholder='Qidirish'
+                                onFocus={(e) => {
+                                    if (e.target.value === '') {
+                                        setSearchedProducts([]);
+                                    }
+                                }}
+                            />
+
+                            <button className="absolute right-6 top-[11px]">
+                                <img width={24} height={24} src={searchIcon} alt="search icon" className="w-6 h-6" />
+                            </button>
+
+                            <ul className="search-results_list">
+                                {
+                                    searchedProducts.map((product, index) => {
+                                        return (
+                                            <li key={index}>
+                                                <Link to={`/product/${product.name.toLowerCase().replace(/\s+/g, '-')}`} className="search-results_list_item">
+                                                    <img src={product.images[0].src} alt="" className="search-results_list_item_img" />
+                                                    <h3 className="search-results_list_item_title">{product.name}</h3>
+                                                </Link>
+                                            </li>
+                                        )
+                                    })
+                                }
+                            </ul>
+                        </form>
+                    </div>
 
                     {/* login, register wrapper, search form modal button */}
                     <div className="flex-center space-x-6 max-540:space-x-5 max-375:space-x-4">
                         {/* contact page link */}
                         <Link to='/contact' className="flex-center space-x-1">
                             <img width={24} height={24} src={supportIcon} alt="support icon" className="w-6 h-6 max-768:block" />
-                            <span className='max-640:hidden'>Aloqa</span>
+                            <span className='max-860:hidden max-768:block max-640:hidden'>Aloqa</span>
                         </Link>
 
                         {/*  responsive search form modal button */}
