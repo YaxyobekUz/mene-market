0473dc827e5f5d6e8c2461a97ffeca18aa69fa3c
yaxyobekb <pubgn9642@gmail.com>
2024-06-02 18:31:47 +0500

    change register page

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,6 +1,4 @@
 import React, { useState } from "react";
-
-// router dom
 import { Link, useNavigate } from "react-router-dom";
 
 // axios
@@ -8,17 +6,17 @@ import axiosConfig from "../api/axios/axios";
 
 // helpers
 import {
-  checkInputValueByRegex,
-  errorNotification,
   getElement,
+  errorNotification,
   successNotification,
+  checkInputValueByRegex,
 } from "../helpers/helpers";
 import { emailRegex, passwordRegex } from "../helpers/regexes";
 
 // images
 import eye from "../assets/images/svg/eye.svg";
-import logo from "../assets/images/other/logo.png";
 import eyeSlash from "../assets/images/svg/eye-slash.svg";
+import DotsLoader from "../components/DotsLoader";
 
 const Register = () => {
   const navigate = useNavigate();
@@ -35,9 +33,9 @@ const Register = () => {
     const elSurnameInput = getElement(e, ".js-surname-input");
     const elPasswordInput = getElement(e, ".js-password-input");
 
-    const name = checkInputValueByRegex(elNameInput);
-    const email = checkInputValueByRegex(elEmailInput, emailRegex);
     const password = checkInputValueByRegex(elPasswordInput, passwordRegex);
+    const email = checkInputValueByRegex(elEmailInput, emailRegex);
+    const name = checkInputValueByRegex(elNameInput);
 
     // check form elements value
     if (!loader && isAgree && email && password && name) {
@@ -79,137 +77,125 @@ const Register = () => {
   };
 
   return (
-    <div className="flex-center justify-center grow container !px-0 max-w-[1480px] h-full">
-      <div className="grid grid-cols-2 h-screen w-full max-768:grid-cols-1">
-        {/* image */}
-        <div className="flex justify-center items-start w-full h-full bg-[url('./assets/images/other/chair.png')] bg-center bg-cover bg-no-repeat max-768:hidden">
-          <Link to="/" className="mt-16">
-            <img
-              width={96}
-              height={48}
-              src={logo}
-              alt="mene market logo"
-              className="w-24 h-12"
-            />
+    <div className="flex justify-center px-16 py-8 h-screen overflow-y-auto w-full max-1024:px-5 max-860:py-10 max-768:h-auto max-768:overflow-y-visible scroll_hidden">
+      <div className="max-w-[456px] w-full my-auto">
+        {/* title */}
+        <h1 className="mb-6">Ro'yxatdan o'tish </h1>
+
+        {/* description */}
+        <p className="text-regular-16 text-primary-gray-500 mb-8">
+          <span>Allaqachon akkuntingiz bormi? </span>
+          <Link to="/auth/login" className="text-primary-blue-700 mb-8">
+            Kirish
           </Link>
-        </div>
+        </p>
 
         {/* register form */}
-        <div className="flex justify-center px-16 py-8 h-screen overflow-y-auto w-full max-1024:px-5 max-860:py-10 max-768:h-auto max-768:overflow-y-visible scroll_hidden">
-          <div className="max-w-[456px] w-full my-auto">
-            <h1 className="mb-6">Ro'yxatdan o'tish </h1>
+        <form onSubmit={register} className="auth-form">
+          {/* name */}
+          <input
+            autoFocus
+            name="name"
+            type="text"
+            autoComplete="off"
+            placeholder="Ismingiz"
+            className="auth-form-input js-name-input"
+          />
 
-            {/* description */}
-            <p className="text-regular-16 text-primary-gray-500 mb-8">
-              <span>Allaqachon akkuntingiz bormi? </span>
-              <Link to="/auth/login" className="text-primary-blue-700 mb-8">
-                Kirish
-              </Link>
-            </p>
+          {/* surname */}
+          <input
+            type="text"
+            name="surname"
+            autoComplete="off"
+            className="auth-form-input js-surname-input"
+            placeholder="Familiyangiz (Ixtiyoriy)"
+          />
 
-            {/* login form */}
-            <form onSubmit={register} className="register-form">
-              {/* name */}
-              <input
-                autoFocus
-                name="name"
-                type="text"
-                autoComplete="off"
-                placeholder="Ismingiz"
-                className="auth-form-input js-name-input"
-              />
+          {/* email */}
+          <input
+            name="email"
+            type="email"
+            disabled={loader}
+            autoComplete="off"
+            placeholder="E-pochta manzilingiz"
+            className="auth-form-input js-email-input"
+          />
 
-              {/* surname */}
-              <input
-                type="text"
-                name="surname"
-                autoComplete="off"
-                className="auth-form-input js-surname-input"
-                placeholder="Familiyangiz (Ixtiyoriy)"
-              />
+          {/* password */}
+          <div className="flex flex-col relative">
+            <input
+              name="password"
+              disabled={loader}
+              autoComplete="off"
+              placeholder="Parol"
+              type={`${passwordInput ? "password" : "text"}`}
+              className="auth-form-input js-password-input pr-10"
+            />
 
-              {/* email */}
-              <input
-                name="email"
-                type="email"
-                autoComplete="off"
-                placeholder="E-pochta manzilingiz"
-                className="auth-form-input js-email-input"
-              />
-
-              {/* password */}
-              <div className="flex flex-col relative">
-                <input
-                  name="password"
-                  autoComplete="off"
-                  placeholder="Parol"
-                  className="auth-form-input js-password-input"
-                  type={`${passwordInput ? "password" : "text"}`}
+            {/* set toggle password input */}
+            <button
+              type="button"
+              onClick={() => setPasswordInput(!passwordInput)}
+              className="absolute top-2 right-0"
+            >
+              {passwordInput ? (
+                <img
+                  src={eye}
+                  width={24}
+                  height={24}
+                  alt="eye icon"
+                  className="w-6 h-6"
                 />
-
-                {/* set toggle password input */}
-                <button
-                  type="button"
-                  onClick={() => setPasswordInput(!passwordInput)}
-                  className="login-form_password-input-wrapper-button"
-                >
-                  {passwordInput ? (
-                    <img
-                      src={eye}
-                      width={24}
-                      height={24}
-                      alt="eye icon"
-                      className="w-6 h-6"
-                    />
-                  ) : (
-                    <img
-                      width={24}
-                      height={24}
-                      src={eyeSlash}
-                      alt="eye slash icon"
-                      className="w-6 h-6"
-                    />
-                  )}
-                </button>
-              </div>
-
-              {/* agree */}
-              <label className="flex-center gap-3 text-regular-16 text-primary-gray-500">
-                <input
-                  type="checkbox"
-                  autoComplete="off"
-                  onChange={(e) => {
-                    e.target.checked ? setIsAgree(true) : setIsAgree(false);
-                  }}
-                  className="register-form_input-checkbox js-checkbox-input"
+              ) : (
+                <img
+                  width={24}
+                  height={24}
+                  src={eyeSlash}
+                  alt="eye slash icon"
+                  className="w-6 h-6"
                 />
-
-                {/* checkbox wrapper */}
-                <div className="!text-base">
-                  <span>Men </span>
-                  <Link className="!text-base text-primary-black-800 font-medium">
-                    Maxfiylik siyosati{" "}
-                  </Link>
-                  <span>va </span>
-                  <Link className="!text-base text-primary-black-800 font-medium">
-                    Foydalanish shartlari{" "}
-                  </Link>
-                  <span>ga roziman.</span>
-                </div>
-              </label>
-
-              {/* submit button */}
-              <button
-                disabled={loader || !isAgree}
-                className={`${
-                  loader || !isAgree ? "opacity-70" : "opacity-100"
-                } flex-center justify-center py-2.5 text-white text-regular-16 w-full rounded-lg bg-primary-black-800 transition-opacity`}
-              >
-                {!loader ? "Ro'yxatdan o'tish" : "Ro'yxatdan o'tilmoqda..."}
-              </button>
-            </form>
+              )}
+            </button>
           </div>
-        </div>
+
+          {/* agree */}
+          <label className="flex-center gap-3 text-regular-16 text-primary-gray-500">
+            <input
+              type="checkbox"
+              autoComplete="off"
+              disabled={loader}
+              onChange={(e) => {
+                if (!loader) {
+                  e.target.checked ? setIsAgree(true) : setIsAgree(false);
+                }
+              }}
+              className="js-checkbox-input w-5 h-5"
+            />
+
+            {/* checkbox wrapper */}
+            <div className="!text-base">
+              <span>Men </span>
+              <Link className="!text-base text-primary-black-800 font-medium">
+                Maxfiylik siyosati{" "}
+              </Link>
+              <span>va </span>
+              <Link className="!text-base text-primary-black-800 font-medium">
+                Foydalanish shartlari{" "}
+              </Link>
+              <span>ga roziman.</span>
+            </div>
+          </label>
+
+          {/* submit button */}
+          <button
+            disabled={loader || !isAgree}
+            className={`${
+              loader || !isAgree ? "opacity-70" : "opacity-100"
+            } flex-center justify-center py-2.5 text-white text-regular-16 w-full rounded-lg bg-primary-black-800 transition-opacity`}
+          >
+            {!loader ? <span>Ro'yxatdan o'tish</span> : <DotsLoader />}
+          </button>
+        </form>
       </div>
     </div>
   );
