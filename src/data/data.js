a99a32ed43479322085a1caa40eb1d9417a5a862
yaxyobekb <pubgn9642@gmail.com>
2024-06-02 18:40:11 +0500

    add product types data

---
+++ src/data/data.js
@@ -0,0 +1,80 @@
+export const productTypesData = [
+  {
+    label: "Elektronika",
+    value: "electronics",
+  },
+  {
+    label: "Maishiy texnika",
+    value: "household-appliances",
+  },
+  {
+    label: "Kiyimlar",
+    value: "clothes",
+  },
+  {
+    label: "Oyoq kiyimlar",
+    value: "shoes",
+  },
+  {
+    label: "Aksessuarlar",
+    value: "accessories",
+  },
+  {
+    label: "Parfumeriya",
+    value: "parfumery",
+  },
+  {
+    label: "Salomatlik",
+    value: "health",
+  },
+  {
+    label: "Uy-ro'zg'or buyumlari",
+    value: "household-items",
+  },
+  {
+    label: "Qurilish jihozlari",
+    value: "construction-equipment",
+  },
+  {
+    label: "Avtomobil jihozlari",
+    value: "automotive-products",
+  },
+  {
+    label: "Bolalar uchun mahsulotlar",
+    value: "products-for-children",
+  },
+  {
+    label: "Sevimli mashg'ulot va ijodkorlik",
+    value: "hobby-and-creativity",
+  },
+  {
+    label: "Sport va dam olish",
+    value: "sports-and-recreation",
+  },
+  {
+    label: "Oziq-ovqat mahsulotlari",
+    value: "food-products",
+  },
+  {
+    label: "Maishiy kimyoviy moddalar",
+    value: "household-chemicals",
+  },
+  {
+    label: "Hayvonlar uchun mahsulotlar",
+    value: "products-for-animals",
+  },
+  {
+    label: "Kitoblar",
+    value: "books",
+  },
+  {
+    label: "Kottej, bog' va uy",
+    value: "garden-and-house",
+  },
+  {
+    label: "Boshqa",
+    value: "other",
+  },
+];
+
+export const imageBaseUrl = "https:/menemarket-8699a792d090.herokuapp.com/";
