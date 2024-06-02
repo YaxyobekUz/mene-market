a7db1b191e23453141f22d6538341be5e03e3278
yaxyobekb <pubgn9642@gmail.com>
2024-06-02 18:39:51 +0500

    format regexes.js file codes

--- src/helpers/regexes.js
+++ src/helpers/regexes.js
@@ -1,4 +1,4 @@
-export const defaultRegex = /^[^\s]+(?:\s+[^\s]+)*$/;
-export const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 export const passwordRegex = /^.{8,}$/;
 export const usernameRegex = /^[a-zA-Z0-9_.-]+$/;
+export const defaultRegex = /^[^\s]+(?:\s+[^\s]+)*$/;
+export const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
