d92d7144468492b76de11912aad4de4ec819706e
yaxyobekb <pubgn9642@gmail.com>
2024-06-02 18:35:25 +0500

    format codes

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
-import './index.css';
-import { Provider } from 'react-redux'
-import { store } from './store/store.js'
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-    <Provider store={store}>
-        <App />
-    </Provider>
+// css
+import "./index.css";
+
+// routers
+import App from "./App.jsx";
+
+// redux
+import { Provider } from "react-redux";
+import { store } from "./store/store.js";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <Provider store={store}>
+    <App />
+  </Provider>
 );
