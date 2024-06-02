3b6c332969ef1ad9d3a86c57ea990908e1cd1850
yaxyobekb <pubgn9642@gmail.com>
2024-06-02 18:36:02 +0500

    remove helpers

--- src/js/regexes.js
+++
@@ -1,4 +0,0 @@
-export const defaultRegex = /^[^\s]+(?:\s+[^\s]+)*$/;
-export const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-export const usernameRegex = /^[a-zA-Z0-9_.-]+$/;
-export const passwordRegex = /^.{8,}$/;
