ec6ee13d180def4a29ead0a64c597c42d4d67621
yaxyobekb <pubgn9642@gmail.com>
2024-06-11 14:44:48 +0500

    format homepage codes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,11 @@
-import React, { useEffect } from "react";
+import React from "react";
+
+// components
+import Faq from "../components/Faq";
 import Hero from "../components/Hero";
 import Categories from "../components/Categories";
 import NewProducts from "../components/NewProducts";
 import PopularProducts from "../components/PopularProducts";
-import Faq from "../components/Faq";
 
 const Home = () => {
   return (
