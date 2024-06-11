90df77ded19d8f9dae1341a5be441bab746f8fe0
yaxyobekb <pubgn9642@gmail.com>
2024-06-11 14:43:23 +0500

    fix auth layout bugs

--- src/layouts/AuthRoot.jsx
+++ src/layouts/AuthRoot.jsx
@@ -1,46 +1,33 @@
 import React from "react";
-import { useSelector } from "react-redux";
-import { Link, Outlet} from "react-router-dom";
+
+// router dom
+import { Link, Outlet } from "react-router-dom";
 
 // images
-import image from "../assets/images/svg/success.svg";
+import logo from "../assets/images/other/logo.png";
 
 const AuthRoot = () => {
-  const { isLoggedIn } = useSelector((store) => store.isLoggedIn);
-  // content
-  if (isLoggedIn) {
-    return (
-      <>
-        {/* content */}
-        <div className="flex items-center gap-8 justify-center min-w-full min-h-screen py-10 max-860:flex-col overflow-y-auto max-860:pt-5">
-          <img
-            width={440}
-            height={440}
-            src={image}
-            title="TIZIMGA KIRGANSIZ"
-            alt="your logged in"
-            className="w-[440px] aspect-square max-540:w-4/5 max-375:w-11/12"
-          />
-          <div className="max-w-sm space-y-5 max-860:px-5 max-860:text-center max-860:max-w-md">
-            <h1>Tizimga muvaffaqiyatli kirdingiz! </h1>
-            <p className="max-375:text-medium-16">
-              <b>Eslatma!</b> Agar siz tizimga kirgan bo'lsangiz tizimdan
-              chiqmasdan qayta ro'yxatdan o'tolmaysiz yoki boshqa akkauntingizga
-              kirolmaysiz.
-            </p>
-            <Link
-              className="btn-primary_linear-blue py-3.5 px-5 max-860:mx-auto"
-              to="/"
-            >
-              Bosh sahifaga qaytish
-            </Link>
-          </div>
+  return (
+    <div className="flex-center justify-center grow container !px-0 max-w-[1480px] h-full">
+      <div className="grid grid-cols-2 h-screen w-full max-768:grid-cols-1">
+        {/* image */}
+        <div className="flex justify-center items-start w-full h-full bg-[url('./assets/images/other/chair.png')] bg-center bg-cover bg-no-repeat max-768:hidden">
+          <Link to="/" className="mt-16">
+            <img
+              width={96}
+              height={48}
+              src={logo}
+              alt="mene market logo"
+              className="w-24 h-12"
+            />
+          </Link>
         </div>
-      </>
-    );
-  } else {
-    return <Outlet />;
-  }
+
+        {/* pages */}
+        <Outlet />
+      </div>
+    </div>
+  );
 };
 
 export default AuthRoot;
