3b82282fffbb2186c51b2038f5192882e08914b1
yaxyobekb <pubgn9642@gmail.com>
2024-06-12 22:29:55 +0500

    change contact page some contents language and fix some bugs

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,108 +1,164 @@
-import React, { useEffect } from 'react';
+import React from "react";
 
 // components
-import FaqMainContent from '../components/FaqMainContent';
+import Advantage from "../components/Advantage";
 
 // images
-import shop from '../assets/images/svg/shop.svg';
-import call from '../assets/images/svg/call.svg';
-import sms from '../assets/images/svg/sms.svg';
-
+import sms from "../assets/images/svg/sms.svg";
+import shop from "../assets/images/svg/shop.svg";
+import call from "../assets/images/svg/call.svg";
 
 const Contact = () => {
-    return (
-        <div>
-            {/* main content */}
-            <div className="container">
-                <h1 className="mb-6 text-center">Contact Us</h1>
+  return (
+    <>
+      {/* main content */}
+      <div className="container">
+        <h1 className="mb-6 text-center">Aloqa</h1>
 
-                <div className="grid grid-cols-3 gap-6 max-768:grid-cols-2 max-540:grid-cols-1 mb-12 max-768:mb-10 max-450:mb-8">
-                    {/* item */}
-                    <div className="flex-center flex-col p-6 space-y-4 rounded-lg bg-primary-gray-50 text-center">
-                        <img width={32} height={32} src={shop} alt="shop" className="w-8 h-8" />
-                        <div className="space-y-2 max-w-[280px]">
-                            <h3 className="text-primary-gray-500">Address</h3>
-                            <address className='text-regular-16'>120, Chinobod, Andijon, O’zbekiston</address>
-                        </div>
-                    </div>
+        {/* list */}
+        <ul className="grid grid-cols-3 gap-6 max-768:grid-cols-2 max-540:grid-cols-1 mb-12 max-450:mb-8">
+          {/* 1 */}
+          <li className="flex-center flex-col p-6 space-y-4 rounded-lg bg-primary-gray-50 text-center">
+            <img
+              src={shop}
+              alt="shop"
+              width={32}
+              height={32}
+              className="w-8 h-8"
+            />
 
-                    {/* item */}
-                    <div className="flex-center flex-col p-6 space-y-4 rounded-lg bg-primary-gray-50 text-center">
-                        <img width={32} height={32} src={call} alt="telephone" className="w-8 h-8" />
-                        <div className="space-y-2 max-w-[280px]">
-                            <h3 className="text-primary-gray-500">Contact us</h3>
-                            <a href='tel:+998998745678' target='_blank' className='block text-regular-16'>+998 99 874 56 78</a>
-                        </div>
-                    </div>
+            {/* content body */}
+            <div className="space-y-2 max-w-[280px]">
+              {/* title */}
+              <h3 className="text-primary-gray-500">Manzil</h3>
 
-                    {/* item */}
-                    <div className="flex-center flex-col p-6 space-y-4 rounded-lg bg-primary-gray-50 text-center max-768:col-start-1 max-768:col-end-3 max-540:col-end-2">
-                        <img width={32} height={32} src={sms} alt="email" className="w-8 h-8" />
-                        <div className="space-y-2 max-w-[280px]">
-                            <h3 className="text-primary-gray-500">Email</h3>
-                            <a href='mailto:hello@example.com' target='_blank' className='block text-regular-16'>hello@example.com</a>
-                        </div>
-                    </div>
-                </div>
-
-                {/* send a message */}
-                <div className="grid grid-cols-2 gap-6 mb-16 max-1024:mb-14 max-768:mb-10 max-640:grid-cols-1 max-640:flex max-640:flex-col-reverse max-450:mb-8">
-                    <form className="contact-page_form">
-                        <label className='contact-page_form_input-wrapper'>
-                            <span className="contact-page_form_label-text">Full Name</span>
-                            <input
-                                className='contact-page_form_input'
-                                type="text"
-                                placeholder='Your Name'
-                                name='user name'
-                                required
-                            />
-                        </label>
-
-                        <label className='contact-page_form_input-wrapper'>
-                            <span className="contact-page_form_label-text">Email address</span>
-                            <input
-                                className='contact-page_form_input'
-                                type="email"
-                                placeholder='Your Email'
-                                name='user email'
-                                required
-                            />
-                        </label>
-
-                        <label className='contact-page_form_input-wrapper'>
-                            <span className="contact-page_form_label-text">Message</span>
-                            <textarea
-                                className='contact-page_form_input p-4 h-28 resize-none'
-                                placeholder='Your message'
-                                name='user message'
-                                required
-                            >
-                            </textarea>
-                        </label>
-                        <button className='contact-page_form_button'>
-                            <span className="text-regular-16">Send Message</span>
-                        </button>
-                    </form>
-
-                    {/* map */}
-                    <iframe
-                        className='w-full h-full max-640:h-80'
-                        src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d859.8233566377858!2d71.97749393807459!3d40.87637480506904!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e1!3m2!1sru!2s!4v1705252709584!5m2!1sru!2s"
-                        loading="lazy"
-                        referrerPolicy="no-referrer-when-downgrade"
-                    ></iframe>
-                </div>
+              {/* description */}
+              <address className="text-regular-16">
+                O'zbekiston, Toshkent.sh
+              </address>
             </div>
+          </li>
 
-            {/* sub content */}
-            <div className="py-14 bg-primary-gray-50">
-                <div className="container">
-                    <FaqMainContent />
-                </div>
+          {/* 2 */}
+          <li className="flex-center flex-col p-6 space-y-4 rounded-lg bg-primary-gray-50 text-center">
+            <img
+              width={32}
+              height={32}
+              src={call}
+              alt="telephone"
+              className="w-8 h-8"
+            />
+
+            {/* content body */}
+            <div className="space-y-2 max-w-[280px]">
+              {/* title */}
+              <h3 className="text-primary-gray-500">Biz bilan bog'lanish</h3>
+
+              {/* description */}
+              <a href="tel:+998998745678" className="block text-regular-16">
+                +998 (99)-874-5678
+              </a>
             </div>
+          </li>
+
+          {/* 3 */}
+          <li className="flex-center flex-col p-6 space-y-4 rounded-lg bg-primary-gray-50 text-center max-768:col-start-1 max-768:col-end-3 max-540:col-end-2">
+            <img
+              src={sms}
+              width={32}
+              height={32}
+              alt="email icon"
+              className="w-8 h-8"
+            />
+
+            {/* content body */}
+            <div className="space-y-2 max-w-[280px]">
+              {/* title */}
+              <h3 className="text-primary-gray-500">E-pochta</h3>
+
+              {/* description */}
+              <a
+                target="_blank"
+                href="mailto:example@email.com"
+                className="block text-regular-16"
+              >
+                example@email.com
+              </a>
+            </div>
+          </li>
+        </ul>
+
+        {/* contact form wrapper */}
+        <div className="grid grid-cols-2 gap-6 mb-16 max-1024:mb-14 max-640:grid-cols-1 max-640:flex max-640:flex-col-reverse max-450:mb-8">
+          <form className="space-y-6">
+            {/* name */}
+            <label className="block w-full space-y-3">
+              <span className="text-regular-16 text-primary-gray-500">
+                Ism familiya
+              </span>
+
+              {/* input */}
+              <input
+                type="text"
+                name="user full name"
+                placeholder="Ism familiya"
+                className="w-full rounded-md border border-primary-gray-500 px-4 py-1.5 text-regular-16 placeholder:text-primary-gray-500"
+              />
+            </label>
+
+            {/* email */}
+            <label className="block w-full space-y-3">
+              <span className="text-regular-16 text-primary-gray-500">
+                E-pochta
+              </span>
+
+              {/* input */}
+              <input
+                type="email"
+                placeholder="E-pochta"
+                name="user email address"
+                className="w-full rounded-md border border-primary-gray-500 px-4 py-1.5 text-regular-16 placeholder:text-primary-gray-500"
+              />
+            </label>
+
+            {/* message */}
+            <label className="block w-full space-y-3">
+              <span className="text-regular-16 text-primary-gray-500">
+                Izoh
+              </span>
+
+              {/* input */}
+              <textarea
+                placeholder="Izoh"
+                name="user message"
+                className="w-full rounded-md border border-primary-gray-500 px-4 py-1.5 text-regular-16 placeholder:text-primary-gray-500 p-4 h-28 resize-none"
+              ></textarea>
+            </label>
+
+            {/* submit btn */}
+            <button className="btn-primary_linear-blue justify-center py-1.5 px-10 rounded-lg max-375:w-full max-640:max-w-full">
+              <span className="text-regular-16">Yuborish</span>
+            </button>
+          </form>
+
+          {/* map */}
+          <iframe
+            loading="lazy"
+            className="w-full h-full bg-primary-gray-500 rounded-lg max-640:h-80"
+            referrerPolicy="no-referrer-when-downgrade"
+            src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d859.8233566377858!2d71.97749393807459!3d40.87637480506904!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e1!3m2!1sru!2s!4v1705252709584!5m2!1sru!2s"
+          ></iframe>
         </div>
-    )
+      </div>
+
+      {/* advantage section */}
+      <div className="py-14">
+        <div className="container">
+          <Advantage />
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Contact;
