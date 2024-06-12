e9731f0c4b31aae5abd2c0b65f070a226b6bc550
yaxyobekb <pubgn9642@gmail.com>
2024-06-12 23:38:31 +0500

    change footer some contents

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,77 +1,143 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
+
+// redux
+import { useSelector } from "react-redux";
 
 // images
-import logoImg from '../assets/images/other/logo.png';
-import googlePlayMarketLogo from '../assets/images/svg/google-play.svg';
-import facebookLogo from '../assets/images/svg/facebook.svg';
-import twitterLogo from '../assets/images/svg/twitter.svg';
-import youtubeLogo from '../assets/images/svg/youtube.svg';
+import logoImg from "../assets/images/other/logo.png";
+import twitterLogo from "../assets/images/svg/twitter.svg";
+import youtubeLogo from "../assets/images/svg/youtube.svg";
+import facebookLogo from "../assets/images/svg/facebook.svg";
 
 const Footer = () => {
-    return (
-        <footer className='text-regular-16 bg-primary-black-800 py-8 text-white'>
-            <div className="container">
-                <div className="flex-start-between max-540:flex-col max-540:gap-7">
-                    {/* logo wrapper */}
-                    <div className="flex flex-col">
-                        <Link to="/" className='inline-block mb-4'>
-                            <img src={logoImg} width={152} height={83} alt="logo image" className="w-[152px] h-[83px] max-540:w-28 max-540:h-[57px]" />
-                        </Link>
-                        <span className='flex max-768:flex-col'>
-                            <span>Copyright 2023 ©</span>
-                            <span>All rights reserved</span>
-                        </span>
-                    </div>
+  const { isLoggedIn } = useSelector((store) => store.isLoggedIn);
 
-                    {/* company */}
-                    <ul className="space-y-2">
-                        <li>
-                            <Link>Mening profilim</Link>
-                        </li>
-                        <li>
-                            <Link>Sozlamlar</Link>
-                        </li>
-                        <li>
-                            <Link to='/contact'>Aloqa</Link>
-                        </li>
-                        <li>
-                            <Link to='/public-offer'>Ommaviy offerta</Link>
-                        </li>
-                    </ul>
+  return (
+    <footer className="text-regular-16 bg-primary-black-800 py-8 text-white">
+      <div className="container">
+        <div className="flex-start-between max-540:flex-col max-540:gap-7">
+          {/* logo wrapper */}
+          <div className="flex flex-col gap-2">
+            <Link title="mene market logo" to="/">
+              <img
+                width={96}
+                height={48}
+                src={logoImg}
+                className="w-24 h-12"
+                alt="mene market logo"
+              />
+            </Link>
 
-                    {/* contact */}
-                    <div className="flex flex-col space-y-2">
-                        <div>Aloqa uchun:</div>
-                        <a href="tel:+998998765432" target='_blank'>+998 99 876 54 32</a>
-                        <a href="mailto:example@gmail.com" target='_blank'>example@gmail.com</a>
-                        <ul className="flex space-x-2.5">
-                            <li>
-                                <a href="https://play.google.com/store/apps?hl=ru&gl=US" target='_blank' aria-label='google play market link'>
-                                    <img width={24} height={24} src={googlePlayMarketLogo} alt="logo" className="w-6 h-6" />
-                                </a>
-                            </li>
-                            <li>
-                                <a href="https://www.facebook.com" target='_blank' aria-label='facebook link'>
-                                    <img width={24} height={24} src={facebookLogo} alt="logo" className="w-6 h-6" />
-                                </a>
-                            </li>
-                            <li>
-                                <a href="https://x.com" target='_blank' aria-label='twitter link'>
-                                    <img width={24} height={24} src={twitterLogo} alt="logo" className="w-6 h-6" />
-                                </a>
-                            </li>
-                            <li>
-                                <a href="https://www.youtube.com/" target='_blank' aria-label='youtube link'>
-                                    <img width={24} height={24} src={youtubeLogo} alt="logo" className="w-6 h-6" />
-                                </a>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
-        </footer>
-    )
+            <span className="flex gap-2 opacity-70 font-normal max-768:flex-col">
+              <span>© 2023 - 2024</span>
+              <span>Mene Market</span>
+            </span>
+          </div>
+
+          {/* company */}
+          <div className="flex flex-col gap-2">
+            <h3 className="text-medium-18 text-white">Kompaniya</h3>
+
+            {/* list */}
+            <ul className="space-y-2 opacity-70">
+              <li>
+                <Link to="/">Bosh sahifa</Link>
+              </li>
+
+              {isLoggedIn && (
+                <li>
+                  <Link to="/admin">Admin</Link>
+                </li>
+              )}
+
+              <li>
+                <Link to="/products/all">Mahsulotlar</Link>
+              </li>
+
+              <li>
+                <Link to="/contact">Aloqa</Link>
+              </li>
+
+              <li>
+                <Link>Ommaviy oferta</Link>
+              </li>
+            </ul>
+          </div>
+
+          {/* contact */}
+          <div className="flex flex-col gap-2">
+            <h3 className="text-medium-18 text-white">Aloqa</h3>
+
+            {/* email */}
+            <a
+              target="_blank"
+              className="opacity-70"
+              href="mailto:example@email.com"
+            >
+              example@email.com
+            </a>
+
+            {/* telephone number */}
+            <a className="opacity-70" href="tel:+998998745678">
+              +998 (99)-874-5678
+            </a>
+
+            {/* social networks */}
+            <ul className="flex space-x-2.5">
+              <li>
+                <a
+                  target="_blank"
+                  aria-label="facebook link"
+                  href="https://www.facebook.com"
+                >
+                  <img
+                    width={24}
+                    height={24}
+                    alt="facebook logo"
+                    src={facebookLogo}
+                    className="w-6 h-6"
+                  />
+                </a>
+              </li>
+
+              <li>
+                <a
+                  target="_blank"
+                  href="https://x.com"
+                  aria-label="twitter link"
+                >
+                  <img
+                    width={24}
+                    height={24}
+                    src={twitterLogo}
+                    alt="twitter logo"
+                    className="w-6 h-6"
+                  />
+                </a>
+              </li>
+
+              <li>
+                <a
+                  target="_blank"
+                  aria-label="youtube link"
+                  href="https://www.youtube.com/"
+                >
+                  <img
+                    width={24}
+                    height={24}
+                    src={youtubeLogo}
+                    alt="youtube logo"
+                    className="w-6 h-6"
+                  />
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
