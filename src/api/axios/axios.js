9e2be2b6d2a6593cf21be570599e98646ca39bf5
yaxyobekb <pubgn9642@gmail.com>
2024-06-17 22:28:51 +0500

    update axios config

--- src/api/axios/axios.js
+++ src/api/axios/axios.js
@@ -1,17 +1,17 @@
 import axios from "axios";
 const authData = JSON.parse(localStorage.getItem("auth"));
+export const apiBaseUrl = "https://menemarket-8699a792d090.herokuapp.com/api";
 
 // create Axios instance
 const axiosConfig = axios.create({
-  baseURL: "https://menemarket-8699a792d090.herokuapp.com/api",
+  baseURL: apiBaseUrl,
 });
 
 // Add a request interceptor
 axiosConfig.interceptors.request.use(
   (config) => {
-    config.headers["Authorization"] = `Bearer ${
-      authData ? authData.token : ""
-    }`;
+    const data = JSON.parse(localStorage.getItem("auth"));
+    config.headers["Authorization"] = `Bearer ${data ? data.token : ""}`;
     return config;
   },
   (error) => {
