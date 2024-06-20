7f4ca09c193b0e90f8fa323beb0f5b889e4974bd
yaxyobekb <pubgn9642@gmail.com>
2024-06-20 20:22:40 +0500

    update flow page contents

--- src/pages/Flow.jsx
+++ src/pages/Flow.jsx
@@ -1,157 +1,231 @@
 import React from "react";
+import { Link } from "react-router-dom";
+
+// antd
+import "../css/antd.css";
+import { Dropdown } from "antd";
+
+// redux
 import { useSelector } from "react-redux";
 
-// images
-import search from "../assets/images/svg/search-icon-white.svg";
-import trash from "../assets/images/svg/trash.svg";
-import copy from "../assets/images/svg/copy.svg";
-import { Link } from "react-router-dom";
-const Flow = () => {
-  const { userData } = useSelector((store) => store.userData);
+// components
+import CopyBtn from "../components/CopyBtn";
 
+// images
+import trash from "../assets/images/svg/trash.svg";
+import eyeIcon from "../assets/images/svg/eye-white.svg";
+import dotsIcon from "../assets/images/svg/dots-vertical.svg";
+import searchIcon from "../assets/images/svg/search-icon-white.svg";
+import statisticsIcon from "../assets/images/svg/statistics-solid.svg";
+
+const Flow = () => {
+  const userData = useSelector((store) => store.userData);
   return (
-    <div className="w-full">
-      <div className="admin_page-body">
-        {/* content header */}
-        <div className="flex-center-between gap-5 mb-6 max-640:flex-col max-640:items-start">
-          <h1 className="text-medium-18">Havolalar</h1>
-          <div className="admin_page-body_input-wrapper">
-            <input
-              placeholder="Qidirish"
-              type="text"
-              className="admin_page-body_input"
-            />
+    <div className="admin-page-body space-y-7">
+      {/* page header */}
+      <div className="flex-center-between gap-5 max-640:flex-col max-640:items-start">
+        <h1 className="text-medium-18">Oqim</h1>
+
+        <form
+          title="search"
+          aria-label="search"
+          className="admin-page-search-input-wrapper"
+        >
+          <input
+            type="search"
+            name="search"
+            placeholder="Qidirish"
+            className="admin_page-body_input"
+          />
+
+          {/* submit btn */}
+          <button title="submit" aria-label="search form submit button">
             <img
               width={24}
               height={24}
-              src={search}
+              src={searchIcon}
               alt="search icon"
               className="w-6 h-6"
             />
-          </div>
-        </div>
-
-        {/* main content */}
-        <div className="w-full overflow-x-auto">
-          <table className="block min-w-[1192px]">
-            <thead>
-              <tr>
-                <th className="w-1/6 !px-2">Havola nomi</th>
-                <th className="w-1/6 !px-2">Havola</th>
-                <th className="w-1/6 !px-2">Mahsulot nomi</th>
-                <th className="w-[12%] !px-2">Yaratilgan sana</th>
-                <th className="w-[20%] !px-2">Lorem, ipsum</th>
-                <th className="w-1/6 !px-2">Holati</th>
-              </tr>
-            </thead>
-            <tbody>
-              {userData &&
-              userData.offerLinks &&
-              userData.offerLinks.length > 0 ? (
-                <tr>
-                  <td className="w-1/6 !px-2">Choper</td>
-                  <td className="w-1/6 !px-2">
-                    <button
-                      title="copy"
-                      className="flex-center rounded-sm group focus:outline-none"
-                    >
-                      <span className="inline-block">
-                        <span className="inline-block group-focus:underline">
-                          https://
-                        </span>
-                        <span className="inline-block group-focus:underline">
-                          menemarket/
-                        </span>
-                        <span className="inline-block group-focus:underline">
-                          oqim/
-                        </span>
-                        <span className="inline-block group-focus:underline">
-                          5345345
-                        </span>
-                      </span>
-                      <img
-                        width={24}
-                        height={24}
-                        src={copy}
-                        alt="copy"
-                        className="shrink-0 w-6 h-6"
-                      />
-                    </button>
-                  </td>
-                  <td className="w-1/6 !px-2">Ko’p nasatkali choper</td>
-                  <td className="w-[12%] !px-2">12/01/24</td>
-                  <td className="w-[20%] !px-2">
-                    <label className="flex-center gap-2">
-                      <input type="checkbox" className="peer hidden w-0 h-0" />
-
-                      <svg
-                        width="24"
-                        height="24"
-                        viewBox="0 0 24 24"
-                        fill="none"
-                        xmlns="http://www.w3.org/2000/svg"
-                        className="shrink-0 w-6 h-6 stroke-white peer-checked:stroke-primary-blue-700 peer-checked:fill-white"
-                      >
-                        <path
-                          d="M9 22H15C20 22 22 20 22 15V9C22 4 20 2 15 2H9C4 2 2 4 2 9V15C2 20 4 22 9 22Z"
-                          stroke="white"
-                          strokeWidth="1.5"
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                        />
-                        <path
-                          d="M7.75 11.9999L10.58 14.8299L16.25 9.16992"
-                          strokeWidth="1.5"
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                        />
-                      </svg>
-
-                      <span>So'rovlarni hududsiz qabul qilish</span>
-                    </label>
-                  </td>
-                  <td className="flex gap-4 w-1/6 !px-2">
-                    <div title="active" className="btn-success">
-                      100%
-                    </div>
-                    <button
-                      aria-label="delete"
-                      title="delete"
-                      className="btn-danger !p-[9px]"
-                    >
-                      <img
-                        width={24}
-                        height={24}
-                        src={trash}
-                        alt="delete"
-                        className="w-6 h-6"
-                      />
-                    </button>
-                  </td>
-                </tr>
-              ) : (
-                <tr>
-                  <td className="block space-x-2">
-                    <span className="text-primary-gray-500">
-                      Siz hali hech qanday oqim yaratmadingiz. Oqim yaratish
-                      uchun
-                    </span>
-                    <Link
-                      to="/admin/market"
-                      className="underline text-primary-blue-700"
-                    >
-                      Market
-                    </Link>
-                    <span className="text-primary-gray-500">
-                      sahifasiga kiring
-                    </span>
-                  </td>
-                </tr>
-              )}
-            </tbody>
-          </table>
-        </div>
+          </button>
+        </form>
       </div>
+
+      {/* page body (main content) */}
+      <section className="w-full space-y-5">
+        <h2 className="text-lg font-medium text-primary-gray-500">Havolalar</h2>
+
+        {/* divider */}
+        <div className="divider"></div>
+
+        {/* offer links list */}
+        <ul>
+          {userData.offerLinksData.map((offerLink) => {
+            return (
+              <li
+                key={offerLink.offerLinkId}
+                className="flex-center-between gap-4 py-3.5 border-b border-primary-gray-500 last:border-b-0 max-450:gap-3"
+              >
+                {/* offer link title */}
+                <h3 className="grow text-regular-16 line-clamp-1">
+                  {offerLink.name} Lorem ipsum dolor, sit amet consectetur
+                  adipisicing elit. Ipsam perspiciatis hic quam cumque atque
+                  eum, neque sit nobis inventore aperiam saepe? Impedit ipsam
+                  amet totam, minus culpa dolore voluptates laudantium.
+                </h3>
+
+                {/* content actions */}
+                <div className="flex-center gap-5 shrink-0 max-640:gap-4 max-450:gap-3">
+                  {/* copy btn */}
+                  <CopyBtn
+                    darkTheme={true}
+                    className="flex-center gap-2 max-1240:hidden"
+                    text={
+                      "https://mene-market.netlify.app/flow/product/" +
+                      offerLink.offerLinkId
+                    }
+                  >
+                    <span className="text-regular-16">
+                      Havoladan buferga nusxa olish
+                    </span>
+                  </CopyBtn>
+
+                  {/* product link */}
+                  <Link
+                    className="flex-center gap-2 max-768:hidden"
+                    to={"/products/product/" + offerLink.productId}
+                  >
+                    <img
+                      src={eyeIcon}
+                      width={24}
+                      height={24}
+                      alt="product link icon"
+                      className="w-6 h-6"
+                    />
+
+                    <span className="text-regular-16">Mahsulotni ko'rish</span>
+                  </Link>
+
+                  {/* statistics link */}
+                  <Link
+                    className="flex-center gap-2 max-540:hidden"
+                    to={"/admin/statistics/flow/" + offerLink.offerLinkId}
+                  >
+                    <img
+                      width={24}
+                      height={24}
+                      className="w-6 h-6"
+                      src={statisticsIcon}
+                      alt="statistics icon"
+                    />
+
+                    <span className="text-regular-16">Statistika</span>
+                  </Link>
+
+                  {/* delete offer link btn */}
+                  <button
+                    title="delete link"
+                    aria-label="delete button"
+                    className="btn-danger p-1.5"
+                  >
+                    <img
+                      width={24}
+                      height={24}
+                      src={trash}
+                      alt="delete icon"
+                      className="w-6 h-6"
+                    />
+                  </button>
+
+                  {/* responsive mobile kebab btn */}
+                  <Dropdown
+                    trigger={["click"]}
+                    className="hidden max-1240:block"
+                    menu={{
+                      items: [
+                        {
+                          key: 0,
+                          label: (
+                            <CopyBtn
+                              timeout={0}
+                              darkTheme={true}
+                              className="flex-center gap-2 p-2"
+                              notification="Havola buferga nusxa olindi!"
+                              text={
+                                "https://mene-market.netlify.app/flow/product/" +
+                                offerLink.offerLinkId
+                              }
+                            >
+                              <span className="text-regular-16">
+                                Havoldan buferga nusxa olish
+                              </span>
+                            </CopyBtn>
+                          ),
+                        },
+                        {
+                          key: 1,
+                          label: (
+                            <Link
+                              className="hidden gap-2 p-2 max-768:flex-center"
+                              to={"/products/product/" + offerLink.productId}
+                            >
+                              <img
+                                src={eyeIcon}
+                                width={24}
+                                height={24}
+                                alt="product link icon"
+                                className="w-6 h-6"
+                              />
+
+                              <span className="text-regular-16">
+                                Mahsulotni ko'rish
+                              </span>
+                            </Link>
+                          ),
+                        },
+                        {
+                          key: 3,
+                          label: (
+                            <Link
+                              className="hidden gap-2 p-2 max-540:flex-center"
+                              to={
+                                "/admin/statistics/flow/" +
+                                offerLink.offerLinkId
+                              }
+                            >
+                              <img
+                                width={24}
+                                height={24}
+                                className="w-6 h-6"
+                                src={statisticsIcon}
+                                alt="statistics icon"
+                              />
+
+                              <span className="text-regular-16">
+                                Statistika
+                              </span>
+                            </Link>
+                          ),
+                        },
+                      ],
+                    }}
+                  >
+                    <button className="p-1.5 rounded-full transition-colors duration-300 active:bg-white/20">
+                      <img
+                        width={24}
+                        height={24}
+                        src={dotsIcon}
+                        className="w-6 h-6"
+                        alt="dots vertical icon"
+                      />
+                    </button>
+                  </Dropdown>
+                </div>
+              </li>
+            );
+          })}
+        </ul>
+      </section>
     </div>
   );
 };
