06f93c1669f55368bcc2a63c9c529e2452875a84
yaxyobekb <pubgn9642@gmail.com>
2024-06-21 23:41:47 +0500

    add statistics page full contents

--- src/pages/Statistics.jsx
+++ src/pages/Statistics.jsx
@@ -1,90 +1,154 @@
 import React, { useState } from "react";
+import { Link } from "react-router-dom";
+
+// redux
 import { useSelector } from "react-redux";
 
+// helpers
+import { search } from "../helpers/helpers.js";
+
+// components
+import SearchForm from "../components/SearchForm";
+import EmptyData from "../components/EmptyData.jsx";
+
 // images
-import search from "../assets/images/svg/search-icon-white.svg";
-import { Link } from "react-router-dom";
+import flowIcon from "../assets/images/svg/flow.svg";
+
 const Statistics = () => {
-  const { userData } = useSelector((store) => store.userData);
+  const userData = useSelector((store) => store.userData);
+  const offerLinksData = userData.offerLinksData;
+  const [offerLinks, setOfferLinks] = useState(offerLinksData);
+
+  // calculate offer link clients statistics by status type
+  const offerLinkClientsStatistics = (clients) => {
+    if (clients.length > 0) {
+      const statusCount = {};
+      clients.forEach((client) => {
+        const status = client.statusType;
+        statusCount[`status ${status}`] =
+          (statusCount[`status ${status}`] || 0) + 1;
+      });
+      return statusCount;
+    } else {
+      return 0;
+    }
+  };
+
+  // search offer links
+  const searchOfferLinks = (value) => {
+    if (value.length > 0) {
+      const searchedOfferLinks = search(offerLinksData, "name", value);
+      setOfferLinks(searchedOfferLinks);
+    } else if (
+      value.length === 0 &&
+      offerLinks.length !== offerLinksData.length
+    ) {
+      setOfferLinks(offerLinksData);
+    }
+  };
 
   return (
     <div className="w-full">
-      <div className="admin_page-body">
-        {/* content header */}
-        <div className="flex-center-between gap-5 mb-6 max-640:flex-col max-640:items-start">
-          <h1 className="text-medium-18">Statistika</h1>
-          <div className="admin_page-body_input-wrapper">
-            <input
-              placeholder="Qidirish"
-              type="text"
-              className="admin_page-body_input"
-            />
-            <img
-              width={24}
-              height={24}
-              src={search}
-              alt="search icon"
-              className="w-6 h-6"
+      <div className="admin-page-body px-0">
+        {/* page header */}
+        <div className="container">
+          <div className="flex-center-between gap-5 mb-6 max-640:flex-col max-640:items-start">
+            <h1 className="text-medium-18">Oqimlar statistikasi</h1>
+
+            {/* search form */}
+            <SearchForm
+              onChange={searchOfferLinks}
+              className="max-w-md max-640:max-w-full"
             />
           </div>
         </div>
 
-        {/* main content */}
-        <div className="w-full overflow-x-auto">
-          <table className="block min-w-[1192px]">
-            <thead>
-              <tr>
-                <th className="w-[16%] !px-2">Oqim</th>
-                <th className="w-[7%] !px-2">Tashrif</th>
-                <th className="w-[7%] !px-2">Yangi</th>
-                <th className="w-[10%] !px-2">Qabul Qilindi</th>
-                <th className="w-[11%] !px-2">Yetkazilmoqda</th>
-                <th className="w-[8%] !px-2">Yetkazildi</th>
-                <th className="w-[11%] !px-2">Qayta Qo'ngiroq</th>
-                <th className="w-[7%] !px-2">Spam</th>
-                <th className="w-[9%] !px-2">Qaytib Keldi</th>
-                <th className="w-[7%] !px-2">HOLD</th>
-                <th className="w-[7%] !px-2">Arxivlandi</th>
-              </tr>
-            </thead>
-            <tbody>
-              {userData &&
-              userData.offerLinks &&
-              userData.offerLinks.length > 0 ? (
+        {/* page body (main content) */}
+        {/* offer links statistics */}
+        <div className="overflow-x-auto scroll_gray">
+          {offerLinks.length > 0 ? (
+            <table>
+              {/* table header */}
+              <thead>
                 <tr>
-                  <td className="w-[16%] !px-2">Chopper</td>
-                  <td className="w-[7%] !px-2">105</td>
-                  <td className="w-[7%] !px-2">96</td>
-                  <td className="w-[10%] !px-2">65</td>
-                  <td className="w-[11%] !px-2">14</td>
-                  <td className="w-[8%] !px-2">14</td>
-                  <td className="w-[11%] !px-2">12</td>
-                  <td className="w-[7%] !px-2">10</td>
-                  <td className="w-[9%] !px-2">5</td>
-                  <td className="w-[7%] !px-2">5</td>
-                  <td className="w-[7%] !px-2">1</td>
+                  <th className="w-[23%] text-start">Oqim</th>
+                  <th className="w-[10%]">Buyurtmalar</th>
+                  <th className="w-[10%]">Qabul qilindi</th>
+                  <th className="w-[10%]">Yetkazilmoqda</th>
+                  <th className="w-[10%]">Yetkazildi</th>
+                  <th className="w-[10%]">Qaytib Keldi</th>
+                  <th className="w-[10%]">Kutilmoqda</th>
+                  <th className="w-[10%]">Arxivlandi</th>
+                  <th className="w-[7%]">Ba'tafsil</th>
                 </tr>
-              ) : (
-                <tr>
-                  <td className="block space-x-2">
-                    <span className="text-primary-gray-500">
-                      Siz hali hech qanday oqim yaratmadingiz. Oqim yaratish
-                      uchun
-                    </span>
-                    <Link
-                      to="/admin/market"
-                      className="underline text-primary-blue-700"
-                    >
-                      Market
-                    </Link>
-                    <span className="text-primary-gray-500">
-                      sahifasiga kiring
-                    </span>
-                  </td>
-                </tr>
-              )}
-            </tbody>
-          </table>
+              </thead>
+
+              {/* table body */}
+              <tbody>
+                {offerLinks.map((offerLink) => {
+                  const statistics = offerLinkClientsStatistics(
+                    offerLink.clients
+                  );
+                  return (
+                    <tr key={offerLink.offerLinkId}>
+                      <td className="!justify-normal !max-w-[23%] w-full !text-start">
+                        <span className="line-clamp-2">{offerLink.name}</span>
+                      </td>
+                      <td className="w-[10%]">{offerLink.clients.length}</td>
+                      <td className="w-[10%]">{statistics["status 0"] || 0}</td>
+                      <td className="w-[10%]">{statistics["status 1"] || 0}</td>
+                      <td className="w-[10%]">{statistics["status 2"] || 0}</td>
+                      <td className="w-[10%]">{statistics["status 3"] || 0}</td>
+                      <td className="w-[10%]">{statistics["status 4"] || 0}</td>
+                      <td className="w-[10%]">{statistics["status 5"] || 0}</td>
+                      <td className="w-[7%]">
+                        <Link
+                          className="p-4"
+                          title="statistics"
+                          aria-label="statistics"
+                          to={"/admin/statistics/flow/" + offerLink.offerLinkId}
+                        >
+                          <img
+                            width={16}
+                            height={16}
+                            src={flowIcon}
+                            alt="offer link icon"
+                            className="w-4 h-4"
+                          />
+                        </Link>
+                      </td>
+                    </tr>
+                  );
+                })}
+              </tbody>
+            </table>
+          ) : offerLinksData.length > 0 ? (
+            <EmptyData
+              contentSmall={true}
+              className="container"
+              title="Hech qanday oqim topilmadi!"
+              description="Qidiruv natijasiga ko'ra hech qanday oqim topilmadi."
+            />
+          ) : (
+            <EmptyData
+              contentSmall={true}
+              className="container space-y-2"
+              title="Hech qanday oqim mavjud emas"
+              description="Ushbu sahifada hech qanday oqim statistikasi mavjud emas."
+            >
+              <p className="text-base font-normal opacity-80">
+                Siz yaratgan oqimlar statistikasi bu yerda ko'rinadi. <br />
+                Siz hech qanday oqim yaratmagansiz. Oqim yaratmoqchi bo'lsangiz{" "}
+                <Link
+                  to="/admin/market"
+                  className="text-primary-skyblue-500 font-medium underline"
+                >
+                  Market
+                </Link>{" "}
+                sahifasiga tashrif buyuring!
+              </p>
+            </EmptyData>
+          )}
         </div>
       </div>
     </div>
