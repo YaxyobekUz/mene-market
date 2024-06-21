6495cb1bf9cf108c77afc99fede00092ccd4b2be
yaxyobekb <pubgn9642@gmail.com>
2024-06-21 13:00:01 +0500

    add small content feature styles to empty data component

--- src/components/EmptyData.jsx
+++ src/components/EmptyData.jsx
@@ -3,7 +3,13 @@ import React from "react";
 // images
 import packageBox from "../assets/images/svg/package-box.svg";
 
-const EmptyData = ({ children, className, title, description }) => {
+const EmptyData = ({
+  title,
+  children,
+  className,
+  description,
+  contentSmall,
+}) => {
   return (
     <div className={className}>
       {/* main content */}
@@ -14,18 +20,32 @@ const EmptyData = ({ children, className, title, description }) => {
           height={80}
           src={packageBox}
           alt="package box"
-          className="w-20 h-20 max-450:w-16 max-450:h-16"
+          className={
+            !contentSmall ? "w-20 h-20 max-450:w-16 max-450:h-16" : "w-16 h-16"
+          }
         />
 
         {/* title and description */}
         <div className="space-y-0.5">
           {/* title */}
-          <h2 className="text-xl max-450:text-lg">
+          <h2
+            className={
+              !contentSmall
+                ? "text-xl max-450:text-lg leading-6 max-450:font-medium"
+                : "text-lg leading-6 font-medium"
+            }
+          >
             {title ? title : "Ma'lumotlar mavjud emas!"}
           </h2>
 
           {/* description */}
-          <p className="opacity-80 text-lg leading-6 font-medium max-450:text-base max-450:leading-5 max-450:font-normal">
+          <p
+            className={`opacity-80 ${
+              !contentSmall
+                ? "text-lg leading-6 font-medium max-450:text-base max-450:leading-5 max-450:font-normal"
+                : "text-base leading-5 font-normal"
+            }`}
+          >
             {description
               ? description
               : "Ushbu sahifada hech qanday ma'lumot mavjud emas."}
