35067981a6c5980d351be97ada800457574c7194
yaxyobekb <pubgn9642@gmail.com>
2024-06-24 13:38:25 +0500

    add image viewer modal to main root and fix some bugs

--- src/layouts/MainRoot.jsx
+++ src/layouts/MainRoot.jsx
@@ -1,34 +1,44 @@
 import React, { useEffect } from "react";
 import { Outlet, useLocation } from "react-router-dom";
 
+// redux
+import { useSelector } from "react-redux";
+
 // components
 import Header from "../components/Header";
 import Footer from "../components/Footer";
+import ImageViewerModal from "../components/ImageViewerModal";
 
 // toast
-import { ToastContainer, Zoom } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import { ToastContainer, Zoom } from "react-toastify";
 
 const MainRoot = () => {
   const location = useLocation();
-  const firstLocation = location.pathname
-    .split("/")
-    .filter((item) => item !== "")[0];
-
-  const authPage = firstLocation !== "auth";
-  const adminPage = firstLocation !== "admin";
+  const imageViewerModal = useSelector((store) => store.imageViewerModal);
+  const pathArr = location.pathname.split("/").filter((item) => item !== "");
+  const authPage = pathArr[0] === "auth";
+  const adminPage = pathArr[0] === "admin";
 
   // scroll to top
   useEffect(() => {
     window.scrollTo(0, 0);
   }, [location]);
+
   return (
     <div className="flex flex-col min-h-screen">
-      {authPage && adminPage && <Header />}
+      {/* header */}
+      {!authPage && !adminPage && <Header />}
+
+      {/* main */}
       <main className="flex w-full flex-col grow">
         <Outlet />
       </main>
-      {authPage && <Footer />}
+
+      {/* footer */}
+      {!authPage && <Footer />}
+
+      {/* toast notification container */}
       <ToastContainer
         position="bottom-right"
         autoClose={3500}
@@ -39,6 +49,9 @@ const MainRoot = () => {
         theme={"light "}
         transition={Zoom}
       />
+
+      {/* image viewer modal */}
+      {imageViewerModal.isOpen && <ImageViewerModal />}
     </div>
   );
 };
