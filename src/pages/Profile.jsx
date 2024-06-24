9db4407471367ac3a75ca6f000aba3787ddd1c14
yaxyobekb <pubgn9642@gmail.com>
2024-06-24 14:28:39 +0500

    change profile page full contents

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,103 +1,242 @@
 import React, { useState } from "react";
 
-// axios & redux
-import axios from "axios";
-import { useDispatch, useSelector } from "react-redux";
-import userDataSlice, { setUserData } from "../store/slices/userDataSlice";
-
 // helpers
-import { getElement } from "../helpers/helpers";
-import { defaultRegex } from "../helpers/regexes";
+import {
+  getElement,
+  errorNotification,
+  successNotification,
+  checkInputValueByLength,
+} from "../helpers/helpers";
+
+// data
+import { imageBaseUrl } from "../data/data";
+
+// axios
+import axiosConfig from "../api/axios/axios";
+
+// components
+import CopyBtn from "../components/CopyBtn";
+import DotsLoader from "../components/DotsLoader";
+
+// redux
+import {
+  setImageViewerModalData,
+  setOpenImageViewerModal,
+} from "../store/slices/imageViewerModalSlice";
+import { useDispatch, useSelector } from "react-redux";
+import { setUserData } from "../store/slices/userDataSlice";
+
+// images
+import userIcon from "../assets/images/other/user.jpg";
+import cameraIcon from "../assets/images/svg/camera.svg";
 
 const Profile = () => {
-  const [changeUserData, setChangeUserData] = useState(false);
-  const { userData } = useSelector((store) => store.userData);
+  const dispatch = useDispatch();
+  const [loader, setLoader] = useState(false);
+  const userData = useSelector((store) => store.userData);
+  const [userProfileImageBase64Code, setUserProfileImageBase64Code] =
+    useState(null);
 
-  const formatInputValues = (input) => {
-    const value = input.value;
-    const words = value.trim().split(/\s+/);
-    const filteredWords = words.filter((word) => word !== "");
-    const formattedText = filteredWords.join(" ");
-    input.value = formattedText;
+  // open user profile image modal
+  const openImageViewerModal = () => {
+    dispatch(
+      setImageViewerModalData({
+        images: [
+          userData.userData.image
+            ? imageBaseUrl + userData.userData.image
+            : userIcon,
+        ],
+        alt: "user profile image",
+      })
+    );
+
+    dispatch(setOpenImageViewerModal(true));
+  };
+
+  // select user profile image base64 encoded
+  const handleImageSelect = (event) => {
+    const file = event.target.files[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onloadend = () => {
+        setUserProfileImageBase64Code(reader.result.split(",")[1]);
+      };
+      reader.readAsDataURL(file);
+    }
   };
 
   // change user data
-  const onSubmit = (e) => {
-    const nameInput = getElement(e, ".js-name-input");
-    const surnameInput = getElement(e, ".js-surname-input");
+  const changeUserData = (e) => {
+    e.preventDefault();
 
-    // format input values
-    formatInputValues(nameInput);
-    formatInputValues(surnameInput);
+    // inputs
+    const firstNameInput = getElement(e, ".js-first-name-input");
+    const lastNameInput = getElement(e, ".js-last-name-input");
 
     // check input values
-    const validateSurnameInput = validateInput(
-      defaultRegex,
-      surnameInput,
-      "is-invalid"
-    );
-    const validateNameInput = validateInput(
-      defaultRegex,
-      nameInput,
-      "is-invalid"
+    const firstName = checkInputValueByLength(
+      firstNameInput,
+      firstNameInput.value
     );
 
-    if (validateSurnameInput || validateNameInput) {
-      axios.put(
-        "/api/User",
-        {},
-        {
-          header: {
-            Authorization: `Bearer ${userData.token}`,
-          },
-        }
-      );
+    if (!loader && firstName) {
+      setLoader(true);
+      const userCurrentData = userData.userData;
+
+      // form data
+      const formData = {
+        user: {
+          role: userCurrentData.role,
+          image: userCurrentData.image,
+          email: userCurrentData.email,
+          userId: userCurrentData.userId,
+          balance: userCurrentData.balance,
+          password: userCurrentData.password,
+          userChats: userCurrentData.userChats,
+          lastName: lastNameInput.value.trim(),
+          firstName: firstNameInput.value.trim(),
+          isArchived: userCurrentData.isArchived,
+          phoneNumber: userCurrentData.phoneNumber,
+          productsSold: userCurrentData.productsSold,
+          balanceHistorys: userCurrentData.balanceHistorys,
+        },
+        bytes: userProfileImageBase64Code,
+      };
+
+      // send a reuqest
+      axiosConfig
+        .put("/User", formData)
+        .then((res) => {
+          if (res.status === 200) {
+            dispatch(setUserData(res.data));
+            successNotification("Ma'lumotlar muvaffaqiyatli o'zgartirildi");
+          } else errorNotification();
+        })
+        .catch(() => errorNotification.offline())
+        .finally(() => setLoader(false));
     }
   };
 
   return (
     <div className="space-y-6">
-      <h1>Profil</h1>
-      <form
-        onSubmit={(e) => {
-          e.preventDefault();
-          onSubmit(e);
-        }}
-        className="space-y-6"
-      >
-        <label className="block space-y-3">
-          <span className="text-regular-13 text-[#C4C5C4]">Ism</span>
-          <input
-            className="js-name-input"
-            type="text"
-            placeholder="Ism"
-            defaultValue={userData ? userData.firstName : ""}
-          />
-          <span className="hidden text-regular-14 text-primary-red-500 italic !mt-2">
-            Ismingizni kiriting
-          </span>
-        </label>
+      {/* profile section */}
+      <div className="space-y-5 max-450:space-y-4">
+        {/* title wrapper */}
+        <div className="flex-center-between gap-5">
+          <h1>Profil</h1>
 
-        <label className="block space-y-3">
-          <span className="text-regular-13 text-[#C4C5C4]">Familiya</span>
-          <input
-            className="js-surname-input"
-            type="text"
-            placeholder="Familiya(Ixtiyoriy)"
-            defaultValue={userData ? userData.lastName : ""}
-          />
-          <span className="hidden text-regular-14 text-primary-red-500 italic !mt-2">
-            Familiyangizni kiriting
-          </span>
-        </label>
+          {/* actions */}
+          <div className="flex-center gap-3.5 shrink-0">
+            {/* set user profile image */}
+            <label
+              className="p-2 cursor-pointer"
+              title="add user profile image"
+              aria-label="add user profile image"
+            >
+              <img
+                width={24}
+                height={24}
+                src={cameraIcon}
+                alt="camera icon"
+                className="w-6 h-6"
+              />
 
-        <button
-          disabled={!changeUserData}
-          className="btn-primary_linear-blue px-10 py-3 rounded-lg max-640:px-6 max-640:py-2"
-        >
-          <span className="text-regular-16">O’zgarishlarni saqlash</span>
-        </button>
-      </form>
+              {/* file input */}
+              <input
+                type="file"
+                name="image"
+                disabled={loader}
+                className="hidden"
+                onChange={handleImageSelect}
+                accept="image/png, image/jpeg"
+              />
+            </label>
+
+            {/* copy user id btn */}
+            <CopyBtn
+              darkTheme={true}
+              text={userData.userData.userId}
+              className="flex-center gap-2 p-2"
+              notification="Foydalanuvchi ID si buferga nusxa olindi"
+            />
+          </div>
+        </div>
+
+        {/* profile */}
+        <div className="flex-center gap-3.5 bg-white/5 p-5 rounded-lg max-450:gap-4 max-450:p-4">
+          <img
+            width={64}
+            height={64}
+            alt="user profile image"
+            onClick={openImageViewerModal}
+            className="w-16 h-16 bg-primary-gray-500 rounded-full max-450:w-14 max-450:h-14"
+            src={
+              userData.userData.image
+                ? imageBaseUrl + userData.userData.image
+                : userIcon
+            }
+          />
+
+          {/* user full name */}
+          <div className="space-y-2.5 max-450:space-y-1">
+            <h3 className="text-medium-18 line-clamp-1 max-450:text-regular-16 max-450:font-medium">
+              {userData.userData.firstName}{" "}
+              {userData.userData.lastName ? userData.userData.lastName : ""}
+            </h3>
+
+            <p className="text-regular-16 text-primary-gray-500">
+              Foydalanuvchi
+            </p>
+          </div>
+        </div>
+      </div>
+
+      {/* change user data */}
+      <section className="space-y-5 max-450:space-y-4">
+        <h2 className="text-medium-18 max-450:text-regular-16">
+          Ma'lumotlarni o'zgartirish
+        </h2>
+
+        {/* form */}
+        <form onSubmit={changeUserData} className="profile-page-from">
+          {/* first name */}
+          <label>
+            <span>Ism</span>
+
+            <input
+              type="text"
+              maxLength={64}
+              disabled={loader}
+              name="first name"
+              placeholder="Ism"
+              className="js-first-name-input"
+              defaultValue={userData.userData.firstName}
+            />
+          </label>
+
+          {/* last name */}
+          <label>
+            <span>Familiya</span>
+
+            <input
+              type="text"
+              maxLength={64}
+              name="last name"
+              disabled={loader}
+              placeholder="Familiya"
+              className="js-last-name-input"
+              defaultValue={
+                userData.userData.lastName ? userData.userData.lastName : ""
+              }
+            />
+          </label>
+
+          {/* submit btn */}
+          <button disabled={loader}>
+            {!loader ? "O'zgartirish" : <DotsLoader />}
+          </button>
+        </form>
+      </section>
     </div>
   );
 };
