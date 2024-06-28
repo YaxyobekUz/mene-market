58f350db94cd02011b65033e51e2b540cc54598c
yaxyobekb <pubgn9642@gmail.com>
2024-06-28 15:13:23 +0500

    fix competitions page some styles bugs

--- src/pages/Competitions.jsx
+++ src/pages/Competitions.jsx
@@ -2,29 +2,32 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 // images
+import crown from "../assets/images/svg/crown-icon.svg";
 import arrowLeft from "../assets/images/svg/arrow-left.svg";
 import arrowRight from "../assets/images/svg/arrow-right.svg";
-import crown from "../assets/images/svg/crown-icon.svg";
-import market from "../assets/images/svg/market-icon.svg";
+
 const Competitions = () => {
   return (
-    <div>
-      <div className="admin_page-header">
+    <>
+      {/* page header */}
+      <div className="admin-page-header">
         <Link to="/admin/dashboard">
           <img
             width={24}
             height={24}
             src={arrowLeft}
-            alt="arrow left icon image"
             className="w-6 h-6"
+            alt="arrow left icon"
           />
         </Link>
+
+        {/* page title */}
         <h1>Konkurslar</h1>
       </div>
 
+      {/* main */}
       <ul className="space-y-6">
-
-        <li className="flex gap-6 bg-linear-gradient_black-800 backdrop-filter backdrop-blur-120 rounded-20 p-6 max-768:flex-col max-450:p-5 max-450:gap-5 max-375:p-4">
+        <li className="flex gap-6 bg-linear-gradient_black-800 backdrop-blur-120 rounded-20 p-6 max-1024:gap-5 max-1024:p-5 max-768:flex-col max-450:p-4 max-450:gap-4 max-375:p-4">
           <img
             width={440}
             height={248}
@@ -34,97 +37,33 @@ const Competitions = () => {
           />
 
           <div>
-            <h3 className="text-[29px] leading-8 font-semibold mb-4 line-clamp-1 max-640:text-semibold-23 max-450:text-semibold-20 max-450:mb-2 max-375:text-medium-18">
+            <h3 className="text-2xl semibold mb-4 line-clamp-2 max-450:text-semibold-20 max-450:mb-2 max-375:text-medium-18">
               Elektron pichoq chaxi
             </h3>
-            <p className="text-primary-gray-500 mb-2 max-375:text-base">
+            
+            <p className="mb-2 text-primary-gray-500 font-normal max-375:text-base">
               29.11.2023 - 29.12.2023
             </p>
+
             <p className="max-w-lg text-regular-14 text-primary-gray-500 mb-4 line-clamp-3">
               Konkursda yutuqli o'rinlar 5 ta va umumiy mablag' 15,000,000 so'm.
               Konkurs g'oliblari kamida 500 ta maxsulot sotganlar ichidan
               olinadi. Konkursda yutuqli o'rinlar 5 ta va umumiy mablag'
               15,000,000 so'm.
             </p>
-            <div className="space-y-2 mb-4">
-              <div className="flex gap-2">
-                <img
-                  width={24}
-                  height={24}
-                  src={crown}
-                  alt="crown icon"
-                  className="w-6 h-6"
-                />
-                <p className="text-regular-16">10ta</p>
-              </div>
-              <div className="flex gap-2">
-                <img
-                  width={24}
-                  height={24}
-                  src={market}
-                  alt="market icon"
-                  className="w-6 h-6"
-                />
-                <p className="text-regular-16">Mene Market</p>
-              </div>
-            </div>
-            <Link to="#" className="btn-primary_linear-blue rounded-full px-4">
-              <span className="text-regular-14">Batafsil</span>
+
+            <div className="flex gap-2 mb-3">
               <img
                 width={24}
                 height={24}
-                src={arrowRight}
-                alt="arrow right icon"
+                src={crown}
+                alt="crown icon"
                 className="w-6 h-6"
               />
-            </Link>
-          </div>
-        </li>
-
-        <li className="flex gap-6 bg-linear-gradient_black-800 backdrop-filter backdrop-blur-120 rounded-20 p-6 max-768:flex-col max-450:p-5 max-450:gap-5 max-375:p-4">
-          <img
-            width={440}
-            height={248}
-            src="https://t4.ftcdn.net/jpg/02/51/83/99/360_F_251839941_KgSySeO2fl7FLz1CFbjdv2l6HCqwdTCo.jpg"
-            alt="competition image"
-            className="w-[440px] h-[256px] bg-primary-black-800 rounded-lg object-cover object-center max-1124:w-80 max-768:w-full max-768:h-80 max-640:h-64 max-540:h-52 max-450:h-44 max-450:rounded-md max-375:h-40"
-          />
-
-          <div>
-            <h3 className="text-[29px] leading-8 font-semibold mb-4 line-clamp-1 max-640:text-semibold-23 max-450:text-semibold-20 max-450:mb-2 max-375:text-medium-18">
-              Elektron pichoq chaxi
-            </h3>
-            <p className="text-primary-gray-500 mb-2 max-375:text-base">
-              29.11.2023 - 29.12.2023
-            </p>
-            <p className="max-w-lg text-regular-14 text-primary-gray-500 mb-4 line-clamp-3">
-              Konkursda yutuqli o'rinlar 5 ta va umumiy mablag' 15,000,000 so'm.
-              Konkurs g'oliblari kamida 500 ta maxsulot sotganlar ichidan
-              olinadi. Konkursda yutuqli o'rinlar 5 ta va umumiy mablag'
-              15,000,000 so'm.
-            </p>
-            <div className="space-y-2 mb-4">
-              <div className="flex gap-2">
-                <img
-                  width={24}
-                  height={24}
-                  src={crown}
-                  alt="crown icon"
-                  className="w-6 h-6"
-                />
-                <p className="text-regular-16">10ta</p>
-              </div>
-              <div className="flex gap-2">
-                <img
-                  width={24}
-                  height={24}
-                  src={market}
-                  alt="market icon"
-                  className="w-6 h-6"
-                />
-                <p className="text-regular-16">Mene Market</p>
-              </div>
+              <p className="text-regular-16">10 ta</p>
             </div>
+
+            {/* link */}
             <Link to="#" className="btn-primary_linear-blue rounded-full px-4">
               <span className="text-regular-14">Batafsil</span>
               <img
@@ -138,7 +77,7 @@ const Competitions = () => {
           </div>
         </li>
       </ul>
-    </div>
+    </>
   );
 };
 
